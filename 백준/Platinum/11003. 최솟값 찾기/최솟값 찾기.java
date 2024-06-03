85f261a0b1bd03f1c3dd46a3886eccd2eb62a617
YoungWoo <128558418+kyw0300@users.noreply.github.com>
2024-06-03 14:34:26 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2220 ms, Memory: 591408 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		int[] answer = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Deque<Node> deq = new LinkedList<>();
+		
+		for (int i = 0; i < N; i++) {
+			int num = arr[i];
+			while (!deq.isEmpty() && deq.getLast().value > num) {
+				deq.removeLast();
+			}
+			
+			deq.addLast(new Node(i, num));
+			if(deq.getFirst().index < i - L + 1) {
+				deq.removeFirst();
+			}
+			
+			bw.write(deq.getFirst().value + " ");
+		}
+		
+		bw.flush();
+		bw.close();
+		
+	}
+	
+	static class Node {
+		private int index;
+		private int value;
+		
+		Node(int index, int value) {
+			this.index = index;
+			this.value = value;
+		}
+	}
+}
