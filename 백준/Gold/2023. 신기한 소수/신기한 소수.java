c55f799f7c7c8f8a30ec7e992795a6ca1b1c846e
YoungWoo <128558418+kyw0300@users.noreply.github.com>
2024-06-15 21:04:09 +0900

    [Gold V] Title: 신기한 소수, Time: 988 ms, Memory: 18524 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+	private static int n;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		dfs(2, 1);
+		dfs(3, 1);
+		dfs(5, 1);
+		dfs(7, 1);
+		
+		sc.close();
+	}
+	
+	private static void dfs(int num, int jarisu) {
+		boolean result = sosu(num);
+		if(!result) {
+			return;
+		} else if(result && jarisu == n) {
+			System.out.println(num);
+		};
+		
+		if(jarisu < n) {
+			for(int i = 1; i <= 9; i++) {
+				if(i%2 == 0) continue;
+				int nextNum = Integer.parseInt(num + "" + i);
+				dfs(nextNum, jarisu+1);
+			}
+		}
+	}
+
+	private static boolean sosu(int num) {
+		for (int i = 2; i < num; i++) {
+			if(num%i == 0) return false;
+		}
+		return true;
+	}
+}
